dc80252ecd957de991086ff337cd60b5f64ff4a9
Alexandre Piel <alexandre.piel@gmail.com>
2020-02-11 21:03:25 +0100

    testest

---
+++ .github/actions/test-crawler-run/index.js
@@ -0,0 +1,29 @@
+const core = require('@actions/core');
+const exec = require('@actions/exec');
+
+async function run() {
+    try {
+        core.info('Run test-crawler');
+        const projectId = core.getInput('projectId');
+
+        // const options = {};
+        // options.listeners = {
+        //     stdout: process.stdout.write,
+        //     stderr: process.stderr.write,
+        // };
+        // options.cwd = './lib';
+        // await exec.exec('node', ['index.js', 'foo=bar'], options);
+
+        if (projectId) {
+            core.info(`Run for project ${projectId}`);
+            await exec.exec(`ROOT_FOLDER=\`pwd\` npx -p test-crawler test-crawler-cli --project ${projectId}`);
+        } else {
+            core.info(`Run for all projects`);
+            await exec.exec(`ROOT_FOLDER=\`pwd\` npx -p test-crawler test-crawler-cli`);
+        }
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+}
+
+run();
